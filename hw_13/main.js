b97a2e7220681f55bc7c8be4363701fdf9f6a3e9
Пётр  Лысенко <petrlysenko@MacBook-Air-Petr.local>
2024-03-22 15:40:56 +0200

    corr task # 1 completed

--- hw_13/main.js
+++ hw_13/main.js
@@ -1,24 +1,22 @@
 /* --- 01 --- */
-
+const head = document.querySelector('head');
+const darkTheme = '<link rel="stylesheet" href="./dark-theme.css"/>';
+head.insertAdjacentHTML('beforeEnd', darkTheme);
+console.log(head);
 
 /* --- 02 --- */
 
-
 /* --- 03 --- */
-
 
 /* --- 04 --- */
 
-
 /* --- 05 --- */
-
 
 /* --- 06 ---
   Задача 06 виконується у файлі new-scripts.js 
 */
 
 /* --- 07 --- */
-
 
 /* --- 08 ---
   Задача 08 виконується у файлі new-scripts.js 
